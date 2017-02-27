ee6f3e5faec53a26a05edc854ff7bf1cc8bdc77f
fontEndEasy <zxl13530046106@sina.cn>
2017-02-28 00:58:37 +0800

    add scripts and style and modify index.html

---
+++ style/global.js
@@ -0,0 +1,26 @@
+import React, {StyleSheet, Dimensions, PixelRatio} from "react-native";
+const {width, height, scale} = Dimensions.get("window"),
+    vw = width / 100,
+    vh = height / 100,
+    vmin = Math.min(vw, vh),
+    vmax = Math.max(vw, vh);
+
+export default StyleSheet.create({
+    "*": {
+        "boxSizing": "border-box",
+        "fontFamily": "microsoft yehei",
+        "fontSize": 16,
+        "paddingTop": 0,
+        "paddingRight": 0,
+        "paddingBottom": 0,
+        "paddingLeft": 0,
+        "marginTop": 0,
+        "marginRight": 0,
+        "marginBottom": 0,
+        "marginLeft": 0
+    },
+    "body": {
+        "minWidth": 760,
+        "maxWidth": 1200
+    }
+});
