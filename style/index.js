05ecba6518a9c50a3fbec25129f46f7fa8bd09c8
fontEndEasy <zxl13530046106@sina.cn>
2017-02-28 01:33:44 +0800

    update style

--- style/index.js
+++ style/index.js
@@ -5,34 +5,4 @@ const {width, height, scale} = Dimensions.get("window"),
     vmin = Math.min(vw, vh),
     vmax = Math.max(vw, vh);
 
-export default StyleSheet.create({
-    "easyui-container": {
-        "marginTop": 0,
-        "marginRight": "auto",
-        "marginBottom": 0,
-        "marginLeft": "auto",
-        "minHeight": 200
-    },
-    "easyui-header": {
-        "marginBottom": 10,
-        "borderRadius": 5,
-        "backgroundColor": "#DEDEDE",
-        "boxShadow": "0 1px 6px #DEDEDE",
-        "marginTop": 10,
-        "height": 45
-    },
-    "easyui-content": {
-        "marginBottom": 10,
-        "borderRadius": 5,
-        "backgroundColor": "#DEDEDE",
-        "boxShadow": "0 1px 6px #DEDEDE",
-        "minHeight": 600
-    },
-    "easyui-footer": {
-        "marginBottom": 10,
-        "borderRadius": 5,
-        "backgroundColor": "#DEDEDE",
-        "boxShadow": "0 1px 6px #DEDEDE",
-        "minHeight": 100
-    }
-});
+export default StyleSheet.create({});
