528525b813814c1efadf6b2415e3de83800227a2
fontEndEasy <zxl13530046106@sina.cn>
2017-02-28 09:36:07 +0800

    update

--- style/global.js
+++ style/global.js
@@ -5,4 +5,22 @@ const {width, height, scale} = Dimensions.get("window"),
     vmin = Math.min(vw, vh),
     vmax = Math.max(vw, vh);
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+    "*": {
+        "boxSizing": "border-box",
+        "fontFamily": "microsoft yehei",
+        "fontSize": 16,
+        "paddingTop": 0,
+        "paddingRight": 0,
+        "paddingBottom": 0,
+        "paddingLeft": 0,
+        "marginTop": 0,
+        "marginRight": 0,
+        "marginBottom": 0,
+        "marginLeft": 0
+    },
+    "easyui-header": {
+        "height": 126,
+        "backgroundColor": "#1791BD"
+    }
+});
