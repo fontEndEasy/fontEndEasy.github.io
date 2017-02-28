e8ffdbd584d168c3007a2b43b94c4445aa669a13
fontEndEasy <zxl13530046106@sina.cn>
2017-02-28 23:52:02 +0800

    update

--- javascripts/index.js
+++ javascripts/index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   setTimeout(function() {
     $('#skill').addClass('skill-animate');
   }, 300);
-  
+
   function getTemplateData(container, source, context) {
     let template = Handlebars.compile(source)
     let html = template(context);
@@ -12,8 +12,8 @@ $(document).ready(function() {
   }
 
   let s0 = $('#entry-template').html();
-  let s1 = $('#easyui-ul').html();
+  // let s1 = $('#easyui-ul').html();
   getTemplateData($('.skill'), s0, skillData);
-  getTemplateData($('.easyui-content'), s1, easyuilists);
+  // getTemplateData($('.easyui-content'), s1, easyuilists);
 
 });
