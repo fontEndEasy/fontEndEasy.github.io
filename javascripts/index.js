60ff23b2d783cab7bb8d76d9ecba72a75f2536a1
fontEndEasy <zxl13530046106@sina.cn>
2017-03-04 18:23:16 +0800

    update

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,44 +1,64 @@
 $(document).ready(function() {
   let loading = $('.loading');
-  
-  $('body').css('overflow-y', 'hidden');
+  let mask = $('.mask');
+  let instroduction = $('.instroduction')
+  let skill = $('.skill');
+  let body = $('body');
+  let win = $(window);
+  let s0 = $('#entry-template').html();
+  let s1 = $('#easyui-ul').html();
+  let uilist, docHeight, scrollHeight, winHeight;
+  getTemplateData($('.skill'), s0, skillData);
+  getTemplateData($('.easyui-content'), s1, easyuilists);
+
   function getTemplateData(container, source, context) {
     let template = Handlebars.compile(source)
     let html = template(context);
     container.html(html);
   }
+  uilist = $('.easyui-list');
+  body.css('overflow-y', 'hidden');
+  win.on('load resize', function() {
 
-  let s0 = $('#entry-template').html();
-  let s1 = $('#easyui-ul').html();
-  getTemplateData($('.skill'), s0, skillData);
-  getTemplateData($('.easyui-content'), s1, easyuilists);
-  var loader = setTimeout(function(){
-    $('.loading').css('transform', 'rotate(360deg)');
+    docHeight = $(document).outerHeight();
+    scrollHeight = $('html,body').scrollTop();
+    winHeight = $(window).outerHeight();
+    mask.css({height: docHeight});
+    loading.css({top:  scrollHeight + (winHeight / 2) });
+  });
+
+  let loader = setTimeout(function(){
+
+    loading.css('transform', 'rotate(360deg)');
 
     let scaler = setTimeout(function() {
-      $('.loading').css('transform', 'rotate(0deg)');
-      $('.loading').fadeOut(1000);
+      loading.css('transform', 'rotate(0deg)');
+      loading.fadeOut(1000);
       clearTimeout(scaler);
       let masker = setTimeout(function() {
-          $('.mask').fadeOut(600);
+          mask.fadeOut(600);
           let skiller = setTimeout(function() {
-            $('.instroduction').animate({opacity: 1}, { duration: 600, complete: function() {
+            instroduction.animate({opacity: 1}, { duration: 600, complete: function() {
 
-              $('.skill').animate({opacity: 1} , {duration: 600, complete: function() {
+              skill.animate({opacity: 1} , {duration: 600, complete: function() {
 
-                $('.easyui-list').each(function(index) {
+                uilist.each(function(index) {
                   let _this = $(this);
                   if(index < 4) {
                       $(this).delay(300*(index+1)).animate({opacity: 1},{duration: 600});
                       $('body').css('overflow-y', 'auto');
                   }else{
-                    $(document).on('scroll', function() {
-                      let getBoundClent = $('.easyui-list')[4].getBoundingClientRect();
-                      let getWinClent = $(window).height();
-                      if(getBoundClent.top <= getWinClent){
-                          _this.delay(300*(index+1)).animate({opacity: 1}, {duration: 600});
-                      }
-                    });
+                    if((uilist[4].getBoundingClientRect()).top <= winHeight){
+                      _this.delay(300*(index+1)).animate({opacity: 1}, {duration: 600});
+                    }else{
+                      $(document).on('scroll', function() {
+                        let getBoundClent = uilist[4].getBoundingClientRect();
+
+                        if(getBoundClent.top <= winHeight){
+                            _this.delay(180*(index+1)).animate({opacity: 1}, {duration: 600});
+                        }
+                      });
+                    }
                   }
                 });
               }});
@@ -50,5 +70,4 @@ $(document).ready(function() {
       clearTimeout(loader);
     }, 2000);
   }, 1000);
-
 });
